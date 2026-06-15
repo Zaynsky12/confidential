34a0b1cc5ebad05482d088848c6362904e537ced
Zayn <123386073+Zaynsky12@users.noreply.github.com>
2026-06-15 14:12:20 +0100

    chore: cleanup repository by removing tracked build artifacts and scratch scripts, update gitignore

--- scratch/testHermes.cjs
+++
@@ -1,6 +0,0 @@
-const id = 'e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43';
-fetch(`https://hermes.pyth.network/v2/updates/price/latest?ids[]=${id}`)
-  .then(r => r.json())
-  .then(data => {
-    console.log("binary data:", data.binary.data);
-  });
