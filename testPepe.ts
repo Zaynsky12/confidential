bcc9ff516bbc312ed2372621d9ee29cb192b98fe
Zayn <123386073+Zaynsky12@users.noreply.github.com>
2026-06-17 09:50:03 +0100

    chore: clean up root directory by moving test scripts to scratch and removing from git tracking

--- testPepe.ts
+++
@@ -1,9 +0,0 @@
-import { gqlClient } from "./src/config/graphql.ts";
-import { gql } from "graphql-request";
-
-async function run() {
-  const query = gql`{ positions(first: 5, orderBy: openedAt, orderDirection: desc) { id trader pairId sizeUsd leverage entryPrice isLong } }`;
-  const data: any = await gqlClient.request(query);
-  console.log(JSON.stringify(data, null, 2));
-}
-run();
